225781236f376a82688fd1048cfb8dcb528377ea
BuildTools <unconfigured@null.spigotmc.org>
2026-04-13 21:13:33 -0700

    Initial commit for Create Class form

---
+++ src/main/java/com/gradetracker/controller/CreateClassController.java
@@ -0,0 +1,10 @@
+/**
+ * @author Robert Mozzetti
+ * created: 4/13/2026
+ * Explanation: Controller for the create class form
+ */
+package com.gradetracker.controller;
+
+public class CreateClassController {
+
+}
