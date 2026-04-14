800b6ef2a09a6e3552a6e56387d40a3c8e957d35
BuildTools <unconfigured@null.spigotmc.org>
2026-04-13 20:14:21 -0700

    Initial commit for Create User form

---
+++ src/main/java/com/gradetracker/controller/CreateUserController.java
@@ -0,0 +1,9 @@
+/**
+ * @author Robert Mozzetti
+ * created: 4/13/2026
+ * Explanation: Controller for the create user form
+ */
+package com.gradetracker.controller;
+
+public class CreateUserController {
+}
