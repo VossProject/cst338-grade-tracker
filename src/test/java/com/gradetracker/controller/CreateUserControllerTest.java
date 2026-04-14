8140ad3f8a019f59315d3df1474655266b6f1eb4
BuildTools <unconfigured@null.spigotmc.org>
2026-04-14 13:32:16 -0700

    Updating tests to use roles table from Olga's database

--- src/test/java/com/gradetracker/controller/CreateUserControllerTest.java
+++ src/test/java/com/gradetracker/controller/CreateUserControllerTest.java
@@ -15,22 +15,22 @@ class CreateUserControllerTest {
     //Tests for valid user creation with a new username, password, and by selecting a role
     @Test
     void testValidUserCreation() {
-        String result = CreateUserController.validate("rmozzetti","Student","password123","password123",Collections.emptySet());
+        String result = CreateUserController.validate("rmozzetti",3,"password123","password123",Collections.emptySet());
         assertNull(result);
     }
 
     //Tests for failed user creation by using a duplicate username
     @Test
     void testDuplicateUsername() {
-        String result = CreateUserController.validate("admin","Admin","password123","password123",Set.of("admin", "teacher1"));
+        String result = CreateUserController.validate("admin",1,"password123","password123",Set.of("admin", "teacher1"));
         assertEquals("That username is already taken.", result);
     }
 
     //Tests for creating 2 users with different usernames but identical passwords (This should not return an error)
     @Test
     void testTwoUsersIdenticalPasswords() {
-        String firstResult = CreateUserController.validate("robert", "Student", "samepassword123","samepassword123",Collections.emptySet());
-        String secondResult = CreateUserController.validate("harvey","Student","samepassword123","samepassword123",Set.of("robert"));
+        String firstResult = CreateUserController.validate("robert", 3, "samepassword123","samepassword123",Collections.emptySet());
+        String secondResult = CreateUserController.validate("harvey",3,"samepassword123","samepassword123",Set.of("robert"));
         assertNull(firstResult);
         assertNull(secondResult);
     }
