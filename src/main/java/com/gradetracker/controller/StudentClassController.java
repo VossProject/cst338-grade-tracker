d3bbe97e10f6b1339d9479f47f181f81f6d8ec7f
Harvey <scilenz.hd@gmail.com>
2026-04-14 22:22:58 -0700

    Initial commit. Updated StudentClassController to include table.

--- src/main/java/com/gradetracker/controller/StudentClassController.java
+++ src/main/java/com/gradetracker/controller/StudentClassController.java
@@ -1,9 +1,18 @@
 package com.gradetracker.controller;
 
+import com.gradetracker.dao.AssignmentDao;
+import com.gradetracker.dao.InMemoryAssignmentDao;
+import com.gradetracker.manager.SceneManager;
+import com.gradetracker.model.Assignment;
+import java.time.LocalDate;
+import javafx.beans.property.SimpleIntegerProperty;
+import javafx.beans.property.SimpleObjectProperty;
+import javafx.beans.property.SimpleStringProperty;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import javafx.stage.Stage;
 
 /**
  * Student class view controller to view assignments.
@@ -13,6 +22,8 @@ import javafx.scene.control.TableView;
  */
 public class StudentClassController {
 
+  private final AssignmentDao assignmentDao = new InMemoryAssignmentDao();
+
   @FXML
   private Label classTitleLabel;
 
@@ -20,27 +31,79 @@ public class StudentClassController {
   private Label classDescriptionLabel;
 
   @FXML
-  private TableView<?> assignmentTable;
+  private TableView<Assignment> assignmentTable;
 
   @FXML
-  private TableColumn<?, ?> titleColumn;
+  private TableColumn<Assignment, String> titleColumn;
 
   @FXML
-  private TableColumn<?, ?> dueDateColumn;
+  private TableColumn<Assignment, LocalDate> dueDateColumn;
 
   @FXML
-  private TableColumn<?, ?> scoreColumn;
+  private TableColumn<Assignment, String> scoreColumn;
 
   @FXML
-  private TableColumn<?, ?> maxGradeColumn;
+  private TableColumn<Assignment, Integer> maxGradeColumn;
+
+  @FXML
+  public void initialize() {
+    classTitleLabel.setText("CST 338 - Software Design");
+    classDescriptionLabel.setText("Current assignments for this class");
+
+    titleColumn.setCellValueFactory(cellData ->
+        new SimpleStringProperty(cellData.getValue().getTitle()));
+
+    dueDateColumn.setCellValueFactory(cellData ->
+        new SimpleObjectProperty<>(cellData.getValue().getDueDate()));
+
+    scoreColumn.setCellValueFactory(cellData ->
+        new SimpleStringProperty("N/A"));
+
+    maxGradeColumn.setCellValueFactory(cellData ->
+        new SimpleIntegerProperty(cellData.getValue().getMaxGrade()).asObject());
+
+    loadSampleAssignments();
+    assignmentTable.getItems().setAll(assignmentDao.findByClassId(1));
+  }
+
+  private void loadSampleAssignments() {
+    assignmentDao.save(new Assignment(
+        "Homework 1",
+        "Intro to JavaFX",
+        LocalDate.of(2026, 4, 20),
+        100,
+        1
+    ));
+
+    assignmentDao.save(new Assignment(
+        "Quiz 1",
+        "Basic MVC concepts",
+        LocalDate.of(2026, 4, 25),
+        20,
+        1
+    ));
+
+    assignmentDao.save(new Assignment(
+        "Project Checkpoint",
+        "Login scene and controller progress",
+        LocalDate.of(2026, 5, 1),
+        50,
+        1
+    ));
+  }
 
   @FXML
   private void handleBack() {
-    // TODO: Navigate back to student dashboard
+    // Temporary: route somewhere that already exists
+    Stage stage = (Stage) assignmentTable.getScene().getWindow();
+    SceneManager sceneManager = new SceneManager(stage);
+    sceneManager.switchScene("/fxml/login.fxml", "Login");
   }
 
   @FXML
   private void handleLogout() {
-    // TODO: Navigate back to login scene
+    Stage stage = (Stage) assignmentTable.getScene().getWindow();
+    SceneManager sceneManager = new SceneManager(stage);
+    sceneManager.switchScene("/fxml/login.fxml", "Login");
   }
 }
