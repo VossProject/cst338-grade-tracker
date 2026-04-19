c503c917cfbc09f2329ebd64998568d9e81786c7
rmozzetti <rmozzetti@csumb.edu>
2026-04-18 19:31:26 -0700

    Updated tests to Integrate DAO.

--- src/test/java/com/gradetracker/controller/CreateUserControllerTest.java
+++ src/test/java/com/gradetracker/controller/CreateUserControllerTest.java
@@ -2,6 +2,8 @@ package com.gradetracker.controller;
 
 import java.util.Collections;
 import java.util.Set;
+import com.gradetracker.dao.InMemoryUserDao;
+import com.gradetracker.dao.UserDao;
 
 import org.junit.jupiter.api.Test;
 
@@ -15,28 +17,29 @@ import static org.junit.jupiter.api.Assertions.assertNull;
  */
 class CreateUserControllerTest {
 
-  /*
   //Tests for valid user creation with a new username, password, and by selecting a role
   @Test
   void testValidUserCreation() {
+    UserDao dao = new InMemoryUserDao();
     String result = CreateUserController.validate(
             "rmozzetti",
             3,
             "password123",
             "password123",
-            Collections.emptySet());
+            dao);
     assertNull(result);
   }
 
   //Tests for failed user creation by using a duplicate username
   @Test
   void testDuplicateUsername() {
+    UserDao dao = new InMemoryUserDao();
     String result = CreateUserController.validate(
             "admin",
             1,
             "password123",
             "password123",
-            Set.of("admin", "teacher1"));
+            dao);
     assertEquals("That username is already taken.", result);
   }
 
@@ -44,20 +47,20 @@ class CreateUserControllerTest {
   //but identical passwords (This should not return an error)
   @Test
   void testTwoUsersIdenticalPasswords() {
+    UserDao dao = new InMemoryUserDao();
     String firstResult = CreateUserController.validate(
             "robert",
             3,
             "samepassword123",
             "samepassword123",
-            Collections.emptySet());
+            dao);
     String secondResult = CreateUserController.validate(
             "harvey",
             3,
             "samepassword123",
             "samepassword123",
-            Set.of("robert"));
+            dao);
     assertNull(firstResult);
     assertNull(secondResult);
   }
-  */
 }
