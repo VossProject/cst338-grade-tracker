974def0e57edf035274fd6d8652bc87185cbf68b
Harvey <scilenz.hd@gmail.com>
2026-04-18 21:00:22 -0700

    Updated header comment for Session.

--- src/main/java/com/gradetracker/manager/Session.java
+++ src/main/java/com/gradetracker/manager/Session.java
@@ -1,7 +1,10 @@
 package com.gradetracker.manager;
 
 /**
- *
+ * Manages the current user session for the application.
+ * Stores the logged-in user's ID and role ID so other controllers
+ * can access user-specific data and behavior throughout the app.
+ * Uses static fields to maintain session state during runtime.
  *
  * @author Harvey Duran
  * @since 04/24/26
